29bb6289381ddca5db7b2d18c84f1afc2b550223
rinij <rinimani7@gmail.com>
2024-10-21 15:10:38 -0600

    constructor function to create objects

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const Person = function( firstName,birthYear) {
+//console.log(this);
+this.firstName = firstName;
+this.bithYear= birthYear;
+
+};
+const jonas = new Person('Jonas', '1991');
+console.log(jonas);
+//new Person('Jonas', '1900');
+
+const matilda = new Person ('Matilda', '1995');
+console.log(matilda);
+
+//1. New empty object is created
+//2. function is called, this = {},this key word is assigned to  an empty object
+//3. {} linked to prototype
+//4. function automatically return {}
